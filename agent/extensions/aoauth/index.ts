a7b5a0e7989eb173281c2440accac62832e2a010
drsh4dow <daniel.morettiv@gmail.com>
2026-07-27 10:07:35 -0400

    added new extension

---
+++ agent/extensions/aoauth/index.ts
@@ -0,0 +1,6 @@
+import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+import { anthropicOAuth } from "./oauth.ts";
+
+export default function aoauthExtension(pi: ExtensionAPI) {
+  pi.registerProvider("anthropic", { oauth: anthropicOAuth });
+}
